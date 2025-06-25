4e202323b66443a5abb992c317887d9e344bf5b1
Andre Rezende <77874276+andre-mr@users.noreply.github.com>
2025-06-25 01:47:39 -0300

    feat: add bytecode plugin to main configuration in electron.vite.config.ts

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,10 +1,14 @@
 import { resolve } from "path";
-import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import {
+  defineConfig,
+  externalizeDepsPlugin,
+  bytecodePlugin,
+} from "electron-vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [externalizeDepsPlugin(), bytecodePlugin()],
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
